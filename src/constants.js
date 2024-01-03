a06030277bc5e61c8d0167c0ef1e8b8da625f292
CAPEL Maxime <83071634+fortyup@users.noreply.github.com>
2024-01-03 20:01:19 +0100

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+// constants.js
+export const CLIENT_ID = "0e8ca648fe6b4565a37f7d258466fbc8";
+export const REDIRECT_URI = "http://localhost:5173/callback";
+export const SPOTIFY_SCOPE = "user-read-private user-read-email";
